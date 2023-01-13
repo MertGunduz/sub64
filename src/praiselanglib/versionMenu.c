f6a09e9a3b3cf246b26b19382d81696fd365c031
MertGunduz <merttgg@gmail.com>
2023-01-13 18:09:50 +0300

    version menu improved

--- src/praiselanglib/versionMenu.c
+++ src/praiselanglib/versionMenu.c
@@ -14,7 +14,7 @@
 /// @brief prints praiselang version related information
 void versionMenu()
 {
-    fprintf(stdout, "praiselang version 1.0.0\n");
+    fprintf(stdout, "praiselang version 1.0.0\n\n");
     fprintf(stdout, "license GPL 3.0: GNU GPL version 3 https://gnu.org/licenses/gpl.html\n");
     fprintf(stdout, "this is a free software: you are free to change and redistribute it.\n");
     fprintf(stdout, "there is no warranty, to the extent permitted by law.\n\n");
